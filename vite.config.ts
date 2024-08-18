d5dcac9f608b006c4711b4fedca3fdbd13fcd930
hhdyhaha <1164549585@qq.com>
2024-08-18 19:11:41 +0800

    fix(vite.config): 修复 base 路径配置问题
    之前由于缺少 base 配置，部署后的应用无法正确解析相对路径，导致资源加载失败。此次更新确保了在生产环境中，base路径被正确设置为根目录。

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from  'tailwindcss'
 import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   css: {
     postcss: {
