96ba6bc4aa5ba417c0265873fc7076132036ab11
hhdyhaha <1164549585@qq.com>
2024-08-18 19:23:41 +0800

    fix(vite.config): 修正构建时目录结构问题
    更改 `vite.config.ts` 中的 `base` 配置，从 `/` 修改为 `./`，以确保在构建时目录结构正确。此更改解决了在生产环境中解析资源路径的问题。

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from  'tailwindcss'
 import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
   css: {
     postcss: {
